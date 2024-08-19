460b6c0c981293192fdb1c02bf591840dca3b4e7
FrancescoDB <fdb4@njit.edu>
2024-08-18 20:49:51 -0400

    added asciivideo application and associated files

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zt!yqer-9^+)qt@3yzc$os$ki_ewrzv*&-4cjfzg0oir2ts)58'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
